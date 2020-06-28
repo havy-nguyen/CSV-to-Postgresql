b8a56b5c216598b4e0813cf437c9b0f0067f1e1e
Ha Nguyen <havyhmn@gmail.com>
2020-06-29 00:28:11 +1000

    all rows written

--- book_csv.py
+++ book_csv.py
@@ -2,10 +2,15 @@ import os
 import csv
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
 
 
 if not os.getenv("DATABASE_URL"):
     raise RuntimeError("DATABASE_URL is not set")
+
+engine = create_engine(os.getenv("DATABASE_URL"))
+database = scoped_session(sessionmaker(bind=engine))
 
 app = Flask(__name__)
 
@@ -16,6 +21,7 @@ db = SQLAlchemy(app)
 
 class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    isbn = db.Column(db.String(30), nullable=False)
     title = db.Column(db.String(150), nullable=False)
     author = db.Column(db.String(120), nullable=False)
     year = db.Column(db.Integer, nullable=False)
@@ -25,16 +31,19 @@ class Book(db.Model):
 
 
 def writeToDatabase():
-  with open('books.csv', 'r') as f:
-    reader = csv.reader(f)
-    next(reader) # Skip the header row
-    for isbn, title, author, year in reader:
-      db.execute
+    with open('books.csv', 'r') as f:
+        reader = csv.reader(f)
+        next(reader) # Skip the header row
+        for isbn, title, author, year in reader:
+            database.execute("INSERT INTO book (isbn, title, author, year) VALUES (:isbn, :title, :author, :year)",
+                        {"isbn": isbn, "title": title, "author": author, "year": year}) 
+    database.commit()
+    
+
+writeToDatabase()
+print("done")
 
 
-
-if __name__ == "__main__":
-    app.run(debug=True)
 
 
 
